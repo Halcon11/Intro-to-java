21099f8ee29f60be0263b937350627523434c4ec
Halcon11 <105290500+Halcon11@users.noreply.github.com>
2022-06-22 21:25:22 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-const operator = prompt("Enter the operation to perform : + , -,  /, *, %");
+const operator = prompt("Enter the operation to perform : + , -,  /, *");
 const number1 = parseFloat(prompt("Enter your first number: "));
 const number2 = parseFloat(prompt("Enter your second number: "));
 
@@ -16,12 +16,8 @@ else if (operator == '/')
 {
     result = number1 / number2;
 }
-else if (operator == '*') 
+else (operator == '*') 
 {
     result = number1 * number2;
-}
-else (operator == '%') 
-{
-    result = number1 % number2;
 }
 prompt(`${number1} ${operator} ${number2} = ${result}`);
